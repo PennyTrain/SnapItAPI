b6ba248e401bfacd5a1cf8b07facfd2980cf78ce
Penny Train <penny.train04@gmail.com>
2024-04-29 15:50:15 +0100

    Edit models in snaplikes and snaps, add files needed in future
    Created snaplikes model and edited snaps model, created urls.py and serializer.py folders in both snaplikes and snaps added the apps to settings.py

--- snaplikes/models.py
+++ snaplikes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from snaps.models import Snap
 
 # Create your models here.
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    snap = models.ForeignKey(Snap, related_name="snaplikes", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+        unique_together = ('owner', 'snap')
+
+    def __str__(self):
+        return str(self.owner) + ' | ' + str(self.snap)
--- snaps/models.py
+++ snaps/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Posted"))
 
 class Snap(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=150, blank=True)
     body = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgyp'
-    )
+    featured_image = models.ImageField(upload_to='images/', default='#')
+    status = models.IntegerField(choices=STATUS, default=1)
+
+    def __str__(self):
+        return str(self.id) + ' | ' + str(self.title)
     
+    class Meta:
+        ordering=["-created"]
