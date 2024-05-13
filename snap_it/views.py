7b5d6504de038a64aefefe35033db2f442d51422
Penny Train <penny.train04@gmail.com>
2024-05-13 12:47:21 +0100

    Bug fix dj rest auth
    Dj-rest-auth has a bug that does not able users to logout therefore I have a new view in the snap_it app that enables this

--- snap_it/views.py
+++ snap_it/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from .settings import (JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE, JWT_AUTH_SECURE)
+from django.http import JsonResponse
+from datetime import datetime
 
 @api_view()
 def root_route(request):
@@ -12,31 +14,26 @@ def root_route(request):
 @api_view(['POST'])
 def logout_route(request):
     """
-    This code defines a view function for logging out users.
-    It handles POST requests, where it creates a response object.
-    Then, it sets two cookies (JWT_AUTH_COOKIE and JWT_AUTH_REFRESH_COOKIE) 
-    with empty values and specific attributes like expiration, security settings,
-    and same-site policy to ensure secure logout. Finally, it returns 
-    the response object with the updated cookies to the client, effectively
-    logging the user out by expiring their authentication tokens.
+    View function for logging out users by expiring authentication tokens.
+    Handles POST requests and returns a JSON response.
     """
-    response = Response()
+    response = JsonResponse({})
     response.set_cookie(
         key=JWT_AUTH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires=datetime(1970, 1, 1),
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='Lax',
+        secure=True,
     )
     response.set_cookie(
         key=JWT_AUTH_REFRESH_COOKIE,
         value='',
         httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        expires=datetime(1970, 1, 1),
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite='Lax',
+        secure=True,
     )
     return response
