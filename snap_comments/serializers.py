8daa281ef733e6b514b800bfa34a3b1495e98e45
Penny Train <penny.train04@gmail.com>
2024-05-21 20:40:43 +0100

    Edit snapcomments as issue creating in them front end
    Edit urls and serializers

--- snap_comments/serializers.py
+++ snap_comments/serializers.py
@@ -3,38 +3,24 @@ from rest_framework import serializers
 from snap_comments.models import SnapComment
 
 class SnapCommentSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the SnapComment model
-    Adds extra fields when returning a list of Comment instances
-    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     created = serializers.SerializerMethodField()
     updated = serializers.SerializerMethodField()
-    attachment = serializers.FileField()
-
 
     def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+        request = self.context.get('request', None)
+        if request:
+            return request.user == obj.owner
+        return False
 
     def get_created(self, obj):
         return naturaltime(obj.created)
 
     def get_updated(self, obj):
         return naturaltime(obj.updated)
-
-    def validate_attachment(self, value):
-        if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Attachment size larger than 2MB!')
-        if hasattr(value, 'image'):
-            if value.image.height > 4096:
-                raise serializers.ValidationError('Image height larger than 4096px!')
-            if value.image.width > 4096:
-                raise serializers.ValidationError('Image width larger than 4096px!')
-        return value
 
     class Meta:
         model = SnapComment
@@ -43,10 +29,3 @@ class SnapCommentSerializer(serializers.ModelSerializer):
             'snap', 'created', 'updated', 'body', 'title', 'is_flagged',
             'pet_name', 'pet_age', 'pet_breed', 'attachment', 'pet_type'
         ]
-
-class SnapCommentDetailSerializer(serializers.ModelSerializer):
-    snap = serializers.ReadOnlyField(source="snap.id")
-
-    class Meta:
-        model = SnapComment
-        fields = '__all__'
